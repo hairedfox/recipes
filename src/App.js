3668cdc154c6ff0d6c67092926e244f33f949aa6
Harefx <hai.redfox@outlook.com>
2019-12-20 00:51:30 +0700

    add file loader to load images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Logo from './static/images/logo.png';
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Hello World</h1>
-    )
-  }
-}
+const App = () => (
+  <div>
+    <h1>Hello World</h1>
+    <img src={Logo} />
+  </div>
+)
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'file-loader'
       }
     ]
   },
